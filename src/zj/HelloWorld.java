4d7d54dd24d0efac5a1ded5de65c18d26a91be0f
webturing <zj@webturing.com>
2019-05-06 18:51:56 +0800

    14 servlet 开发

---
+++ src/zj/HelloWorld.java
@@ -0,0 +1,35 @@
+package zj;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+// 扩展 HttpServlet 类
+
+public class HelloWorld extends HttpServlet {
+
+    private String message;
+
+    public void init() throws ServletException {
+        // 执行必需的初始化
+        message = "Hello World";
+    }
+
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+            throws ServletException, IOException {
+        // 设置响应内容类型
+        response.setContentType("text/html");
+
+        // 实际的逻辑是在这里
+        PrintWriter out = response.getWriter();
+        out.println("<h1>" + message + "</h1>");
+    }
+
+    public void destroy() {
+        // 什么也不做
+    }
+}
